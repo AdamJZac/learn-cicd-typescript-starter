b1d3237fa2de5cd1878234704a2ded86cc5792c4
AdamJZac <adamzachwieja4@gmail.com>
2025-08-17 10:53:51 +0100

    Adding API key unit test

---
+++ src/api/auth.test.ts
@@ -0,0 +1,21 @@
+import { IncomingHttpHeaders } from "http";
+import { describe, expect, test } from "vitest";
+
+const validHeader: IncomingHttpHeaders = {authorization: "apiKey123"};
+const invalidHeader: IncomingHttpHeaders = {};
+
+describe("header", () => {
+  test("Valid header auth is defined", () => {
+    expect(validHeader["authorization"]).toBeDefined();
+  });
+  test("Invalid header auth is not defined", () => {
+    expect(invalidHeader["authorization"]).toBeUndefined();
+  });
+
+  test("Valid header has API key", () => {
+    expect(validHeader["authorization"]?.length).toBeFalsy();
+  });
+  test("Invalid header has no API key", () => {
+    expect(invalidHeader["authorization"]?.length).toBeFalsy();
+  });
+});
